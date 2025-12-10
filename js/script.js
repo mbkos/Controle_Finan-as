5a2fd446fa6ed9bb9f14555d97fc72981e615a1e
mbkos <mariana.kos.ifpr.2023@gmail.com>
2025-12-10 15:28:08 -0300

    colors

--- js/script.js
+++ js/script.js
@@ -86,9 +86,11 @@ function resumo_Financeiro() {
     resumo.appendChild(saldo_p)
 
     if (saldo < 0) {
-        saldo_p.style.backgroundColor = 'Indianred '
+        saldo_p.style.backgroundColor = 'red'
+        saldo_p.style.color = 'white'
     } else if (saldo > 0) {
-        saldo_p.style.backgroundColor = 'Lightgreen'
+        saldo_p.style.backgroundColor = 'green'
+        saldo_p.style.color = 'white'
     }
 
 }
